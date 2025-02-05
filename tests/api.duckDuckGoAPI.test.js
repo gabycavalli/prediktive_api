888317d35e7364a981e9b4ff5b54b2c72097b917
gabycavalli <gabrielcavalli@gmail.com>
2025-02-05 18:45:24 -0300

    deleting-extra-console-logs

--- tests/api.duckDuckGoAPI.test.js
+++ tests/api.duckDuckGoAPI.test.js
@@ -10,38 +10,35 @@ describe('DuckDuckGo API Tests', () => {
     responseData = await duckDuckGoAPI.getSearchResults(API_CONFIG.query);
   });
 
-  test('Validate and count valid Icon URL', () => {
-    const iconCount = duckDuckGoAPI.countIconURLs(responseData);
-
-    console.log(`Total valid Icon URLs found: ${iconCount}`);
-    expect(iconCount).toBeGreaterThan(0); // Make sure that at least one Icon URL is valid
-  });
-
-  test('Validate that RelatedTopics have a valid structure', () => {
+  /* test('Validate that RelatedTopics have a valid structure', () => { --> // to another instance
     const isValidStructure =
       duckDuckGoAPI.validateRelatedTopicsStructure(responseData);
 
     console.log(`The RelatedTopics structure is valid: ${isValidStructure}`);
     expect(isValidStructure).toBe(true);
+  }); */
+
+  test('Should print out values of Icon URLs if it is not null', () => {
+    const validateAndPrintIconURLs = (topics) => {
+      topics.forEach((topic) => {
+        if (topic.Icon?.URL) {
+          console.log(JSON.stringify({ IconURL: topic.Icon.URL }));
+          expect(duckDuckGoAPI.isValidURL(topic.Icon.URL)).toBe(true);
+        }
+
+        if (Array.isArray(topic.Topics)) {
+          validateAndPrintIconURLs(topic.Topics);
+        }
+      });
+    };
+
+    validateAndPrintIconURLs(responseData.RelatedTopics);
   });
 
-  test('Validate and print all Icon URLs have valid formatting', () => {
-    responseData.RelatedTopics.forEach((topic) => {
-      if (topic.Icon?.URL) {
-        console.log(`Icon URL: ${topic.Icon.URL}`);
-        expect(duckDuckGoAPI.isValidURL(topic.Icon.URL)).toBe(true);
-      }
+  test('Should validate and count valid Icon URL', () => {
+    const iconCount = duckDuckGoAPI.countIconURLs(responseData);
 
-      if (topic.Topics) {
-        topic.Topics.forEach((subTopic) => {
-          if (subTopic.Icon?.URL) {
-            console.log(`SubTopic Icon URL: ${subTopic.Icon.URL}`);
-            expect(duckDuckGoAPI.isValidURL(subTopic.Icon.URL)).toBe(true);
-          }
-        });
-      }
-    });
-
-    console.log('All Icon URLs are in a valid format.');
+    console.log(`Total valid Icon URLs found: ${iconCount}`);
+    expect(iconCount).toBeGreaterThan(0); // Make sure that at least one Icon URL is valid
   });
 });
